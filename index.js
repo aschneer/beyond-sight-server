8a1982e767a5b1539db7e21126f11e933ccad3cd
Andrew_Schneer <aschneer738@gmail.com>
2016-01-29 23:52:35 -0500

    Set up working node.js server with express 4.

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer'); // v1.0.5
+var upload = multer(); // for parsing multipart/form-data
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// Routes:
+app.get('/', function(req,res) {
+
+
+	
+});
+
+// Run app:
+app.listen(app.get('port'), function() {
+	console.log('Node app is running on port', app.get('port'));
+});
