d30d0b251d4dfc8f6f545374ffe5e5d90da214a7
Andrew_Schneer <aschneer738@gmail.com>
2016-01-31 20:46:36 -0500

    Everything working well: map, marker, user feedback, database, lat/lng return, etc.

--- public/map.js
+++ public/map.js
@@ -1,9 +1,12 @@
-var timesSquare = {lat: 40.759168, lng: -73.985071};
+var defaultLoc = {lat: 40.759168, lng: -73.985071};
 var map;
 var marker;
 var searchBox;
+var confirmBox;
 var autoComplete;
 var beyondSightServerURL = "http://beyond-sight-server.herokuapp.com";
+var map_marker = new google.maps.Marker({map: null, visible: false});
+var map_infoWindow = new google.maps.InfoWindow({content: ""});
 
 $(document).ready(function() {
 	init();
@@ -14,7 +17,7 @@ function init()
 	// Object containing initial options for the map.
 	var mapOptions = {
 		zoom: 13,
-		center: new google.maps.LatLng(timesSquare.lat, timesSquare.lng),
+		center: new google.maps.LatLng(defaultLoc.lat, defaultLoc.lng),
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	// Create the map object.  This is global.
@@ -22,6 +25,8 @@ function init()
 		mapOptions);
 	// Create search box.
 	searchBox = addSearchBox("Type a location...");
+	// Create confirm box for destination setting feedback.
+	confirmBox = addConfirmBox();
 	// Set up auto complete for places search box.
 	autoComplete = new google.maps.places.Autocomplete(searchBox);
 	// Create event listener for when autocomplete
@@ -29,36 +34,60 @@ function init()
 	google.maps.event.addListener(autoComplete,"place_changed",search);
 }
 
+// Create searchbox for choosing destination.
 function addSearchBox(initialText)
 {
 	// Create the HTML input element.
 	var searchBox = document.getElementById("searchBox");
 	// Add attributes and values to search box.
-	var att1 = document.createAttribute("placeholder");
-	att1.value = initialText;
-	searchBox.setAttributeNode(att1);
+	var att = document.createAttribute("placeholder");
+	att.value = initialText;
+	searchBox.setAttributeNode(att);
 	// Register the search box.
 	// Set the index of the search box input amongst
 	// its siblings in the HTML DOM tree.
-	map.controls[google.maps.ControlPosition.TOP_LEFT].push(searchBox);
+	map.controls[google.maps.ControlPosition.TOP_CENTER].push(searchBox);
 	// Return the search box HTML element
 	// so an event can be run when something
 	// is searched.
 	return searchBox;
 }
 
+// Create box that confirms for the user
+// that the destination has been successfully
+// selected.
+function addConfirmBox()
+{
+	// Create the HTML input element.
+	var confirmBox = document.getElementById("confirmBox");
+	// Register the search box.
+	// Set the index of the search box input amongst
+	// its siblings in the HTML DOM tree.
+	map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(confirmBox);
+	// Return the search box HTML element
+	// so an event can be run when something
+	// is searched.
+	return confirmBox;
+}
+
 function search()
 {
+	// Start by changing the confirm box
+	// back to "failure" state.
+	$("p#confirmBoxText").html("Failure.");
+	// Set background color to red.
+	$("div#confirmBox").css("background-color", "#ff6666");
+	$("div#confirmBox").css("border-style", "none");
 	// Convert search box entry to
 	// latitude and longitude.
 	// This is done using "Place"
 	// library for Google Maps API.
 	var place = autoComplete.getPlace();
 	// Push lat/lng location to database.
-	newDestination(place);
+	setNewDestination(place);
 }
 
-function newDestination(place)
+function setNewDestination(place)
 {
 	var timestamp = new Date(Date.now());
 	var ajaxObj = $.ajax({
@@ -76,7 +105,99 @@ function newDestination(place)
 		success: function(data, textStatus, jqXHR){
 			console.log(data);
 			console.log(textStatus);
+			updateMap();
 		}
+	});
+}
+
+// Update the map with the new destination,
+// and also confirm that the location was
+// successfully stored in the database.
+// If successful, update confirmBox object
+// so user can see that the destination
+// selection was successful.
+function updateMap()
+{
+	var ajaxObj = $.ajax({
+		type: "GET",
+		method: "GET",
+		url: (beyondSightServerURL + "/getdestination"),
+		contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+		dataType: "text",
+		async: true,
+		error: function(jqXHR, textStatus, errorThrown){
+			console.log("Error: " + textStatus);
+			console.log(errorThrown);
+			// Update confirm box with "Failure" message.
+			$("p#confirmBoxText").html("Failure.");
+			// Set confirm box to red.
+			$("div#confirmBox").css("background-color", "#ff6666");
+			$("div#confirmBox").css("border-style", "none");
+		},
+		success: function(data, textStatus, jqXHR){
+			console.log(data);
+			console.log(textStatus);
+			// Get returned data from server/database.
+			var newLat = JSON.parse(data).lat;
+			var newLng = JSON.parse(data).lng;
+			// Add marker at newly set destination.
+			// If a marker already exists, this
+			// should overwrite it.
+			addMarker(newLat, newLng);
+			// Re-center map on this new location.
+			map.setCenter({lat: newLat, lng: newLng});
+			// Update confirm box with "success" message.
+			$("p#confirmBoxText").html("SUCCESS!");
+			// Set confirm box to green.
+			$("div#confirmBox").css("background-color", "#66ff66");
+			$("div#confirmBox").css("border-style", "solid");
+			$("div#confirmBox").css("border-width", "2px");
+			$("div#confirmBox").css("border-color", "black");
+		}
+	});
+}
+
+// Add marker to the destination location.
+function addMarker(myLat, myLng)
+{
+	var location = new google.maps.LatLng({lat: myLat, lng: myLng});
+	// Erase old marker.
+	map_marker.setMap(null);
+	// Define options for my marker.
+	var markerOptions = {
+		map: map,
+		title: "Destination",
+		position: location,
+		visible: true,
+//		icon: "./assets/..."
+	};
+	// Create user's marker object and
+	// update global "map_marker"
+	// with the marker object.
+	map_marker = new google.maps.Marker(markerOptions);
+	// Create an info window
+	// for user's marker and update
+	// global "map_infoWindow" variable.
+	map_infoWindow = new google.maps.InfoWindow({
+		content: ("<h3>"+map_marker.getTitle()+"</h3>"+"<p>"+myLat+", "+myLng+"</p>")
+	});
+	// Open the user's info window
+	// by default.
+	map_infoWindow.open(map,map_marker);
+	// Add a click event listener
+	// for the user's marker.
+	myMarkerClickListener = google.maps.event.addListener(map_marker,"click",function(){
+		map_infoWindow.open(map,map_marker);		
+	});
+	// Center the map on the user's marker.
+	// NOTE: This won't work if the info
+	// windows for all the markers are opened
+	// by default, as they are here.  Comment
+	// that line out in order for this to work.
+//	map.setCenter(map_marker.getPosition());
+	// Add click event listener for info window.
+	google.maps.event.addListener(map_infoWindow,"click",function(){
+		map_infoWindow.open(map, map_marker);
 	});
 }
 
@@ -84,13 +205,8 @@ function newDestination(place)
 
 
 
+
 /*
-
-
-// Add click event listener for info window.
-classMarkerClickListeners[index] = google.maps.event.addListener(classMarkers[index],"click",function(){
-	classInfoWindows[index].open(map,classMarkers[index]);
-});
 
 // Check if data was received.
 if((ajaxObj.readyState == 4) && (ajaxObj.status == 200))
@@ -101,44 +217,5 @@ if((ajaxObj.readyState == 4) && (ajaxObj.status == 200))
 	// the rest of the class.
 	addClassMarkers(parsedData);
 }
-
-// Add marker to the destination location.
-function addUserMarker(LatLngObj)
-{
-	// Define options for my marker.
-	var markerOptions = {
-		map: map,
-		title: myInfo.login,
-		position: LatLngObj,
-		visible: true,
-		icon: "./assets/rickles_noBack_small.png"
-	};
-	// Create user's marker object and
-	// update global "myMarker"
-	// with the marker object.
-	myMarker = new google.maps.Marker(markerOptions);
-	// Create an info window
-	// for user's marker and update
-	// global "myInfoWindow" variable.
-	myInfoWindow = new google.maps.InfoWindow({
-		content: ("<h3>"+myMarker.getTitle()+"</h3>")
-	});
-	// Open the user's info window
-	// by default.
-	myInfoWindow.open(map,myMarker);
-	// Add a click event listener
-	// for the user's marker.
-	myMarkerClickListener = google.maps.event.addListener(myMarker,"click",function(){
-		myInfoWindow.open(map,myMarker);		
-	});
-	// Center the map on the user's marker.
-	// NOTE: This won't work if the info
-	// windows for all the markers are opened
-	// by default, as they are here.  Comment
-	// that line out in order for this to work.
-	map.setCenter(myMarker.getPosition());
-}
-
-
 
 */
